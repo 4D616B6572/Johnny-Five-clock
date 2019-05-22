23458bce44c3b8e3dac58a6e5b565ba7874ec05c
Ryan Hu <b06901191@ntu.edu.tw>
2019-05-22 18:56:53 +0800

    done for the servo control for "a number"

--- Johnny-Five clock/strobe.js
+++ Johnny-Five clock/strobe.js
@@ -1,14 +1,39 @@
-var five = require("johnny-five"),
+let five = require("johnny-five"),
     board = new five.Board();
-
+let servoDigit = [
+    [1, 0, 1, 1, 0, 1, 0, 1, 1, 1], // a (based on the seven segment display)
+    [1, 0, 0, 0, 1, 1, 1, 0, 1, 1], // f
+    [1, 1, 1, 1, 1, 0, 0, 1, 1, 1], // b
+    [0, 0, 1, 1, 1, 1, 1, 0, 1, 1], // g
+    [1, 0, 1, 0, 0, 0, 1, 0, 1, 0], // e
+    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1], // c
+    [1, 0, 1, 1, 0, 1, 1, 0, 1, 0]  // d
+];
+let seconds = 0;
+// this is a test code
+// let servoDigit = [
+//     [1, 0, 1, 1, 0, 1, 0, 1, 1, 1],
+//     [1, 0, 0, 0, 1, 1, 1, 0, 1, 1]
+// ];
 let getTime = () => {
     let date = new Date();
     return date.getSeconds();
 }
-const interval = setInterval(() => {
-    seconds = getTime();
-    // console.log(seconds);
-}, 1000);
+// const interval = setInterval(() => {
+//     seconds = getTime();
+// }, 1000);
+
+const servoMoving = (servo) => {
+    // this is a test code
+    // if (seconds % 3 === 0) {
+    //     servo.to(90, 500, 10);
+    //     servo.to(0, 500, 10);
+    // }
+    let digit = seconds % 10;
+    for (let i = 0; i < servo.length; i++) {
+        servo[i].to((servoDigit[i][digit] ? 90 : 0), 500, 10)
+    }
+}
 
 board.on("ready", function () {
     // Create an Led on pin 13
@@ -18,13 +43,29 @@ board.on("ready", function () {
     for (var i = 5; i <= 11; i++) {
         servo.push(new five.Servo({
             pin: i,
-            range: [55, 145]
+            startAt: 0
         }));
     }
-    this.repl.inject({
-        time: ()=>seconds,
-        on: ()=>{led.on()},
-        off: ()=>{led.off()}
+    // this is a test code
+    // let servo = new five.Servo({
+    //     pin: 6,
+    //     startAt: 0
+    // })
+    servo.filter((element) => { // initial value
+        element.to(90, 500, 10);
+        element.to(0, 500, 10);
     });
-    servo.filter((element) => { element.sweep() });
+
+    this.repl.inject({
+        time: () => seconds,
+        on: () => { led.on() },
+        off: () => { led.off() },
+        sr: () => {
+            return setInterval(() => {
+                seconds = getTime();
+                console.log(seconds);
+                servoMoving(servo);
+            }, 1000);
+        }
+    });
 });
